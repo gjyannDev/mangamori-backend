f39e40eed88ab2642090f8f534697fe4481f2a5e
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-08-14 23:52:40 +0800

    chore: moved the api of discover into it's own file

--- src/services/external/manga.api.ts
+++ src/services/external/manga.api.ts
@@ -1,63 +1,21 @@
 import mangadex from "../api/mangadex.api.js";
 
-export async function getTrendingSeries() {
+export async function getMangaStatistics(mangaIds: string[]) {
   try {
-    const res = await mangadex.get("/manga", {
+    const res = await mangadex.get(`/statistics/manga`, {
       params: {
-        limit: 16,
-        order: {
-          followedCount: "desc",
-        },
-      },
-    });
-    return res.data;
-  } catch (error: any) {
-    console.error(
-      "Error fetching API data: ",
-      error.response?.data || error.message
-    );
-    throw new Error("Error Fetching Data");
-  }
-}
-
-export async function getTopRatedSeries() {
-  try {
-    const res = await mangadex.get("/manga", {
-      params: {
-        limit: 16,
-        order: {
-          rating: "desc",
-        },
+        manga: mangaIds,
       },
     });
 
-    return res.data;
-  } catch (error: any) {
-    console.error(
-      "Error fetching API data: ",
-      error.response?.data || error.message
-    );
-    throw new Error("Error Fetching Data");
+    return res.data.statistics;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Error fetching manga statistics: ", error.message);
+    } else {
+      console.error("Unkown error", error);
+    }
   }
 }
 
-export async function getNewRealeaseSeries() {
-  try {
-    const res = await mangadex.get("/manga", {
-      params: {
-        limit: 16,
-        order: {
-          createdAt: "desc",
-        },
-      },
-    });
-
-    return res.data;
-  } catch (error: any) {
-    console.error(
-      "Error fetching API data: ",
-      error.response?.data || error.message
-    );
-    throw new Error("Error Fetching Data");
-  }
-}
+export async function getMangaCoverImageUrl() {}
